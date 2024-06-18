70edb3d0e9cf6838cb0b15aaf5c95d0d63c607cc
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-19 00:44:12 +0530

    Timer fix partly

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,18 +4,35 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer';
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState('');
   const [userAnswer, setUserAnswer] = useState([]);
-  const activeQuestionIndex = userAnswer.length;
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const activeQuestionIndex =
+    answerState === '' ? userAnswer.length : userAnswer.length - 1;
   const quizComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswer((prevUserAnswers) => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState('answered');
+      setSelectedAnswer(selectedAnswer);
+      setUserAnswer((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState('correct');
+        } else {
+          setAnswerState('wrong');
+        }
+        setTimeout(() => {
+          setAnswerState('');
+          setSelectedAnswer(null); // Reset selected answer
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
+
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
   }, [handleSelectAnswer]);
@@ -28,10 +45,10 @@ export default function Quiz() {
       </div>
     );
   }
+
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => {
-    return Math.random() - 0.5;
-  });
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
@@ -45,9 +62,18 @@ export default function Quiz() {
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => {
+            const isSelected = userAnswer[userAnswer.length - 1] === answer;
+            let cssClasses = '';
+            if (answerState === 'answered' && isSelected) {
+              cssClasses = 'selected';
+            }
             return (
-              <li className="answer" key={answer}>
-                <button onClick={handleSkipAnswer}>{answer}</button>
+              <li className={`answer ${cssClasses}`} key={answer}>
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClasses}>
+                  {answer}
+                </button>
               </li>
             );
           })}
