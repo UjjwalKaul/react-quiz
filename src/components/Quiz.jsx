7c1c948fbad7d4d91e682710d629746ed0bc70c0
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-21 20:29:14 +0530

    Added summary screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useCallback } from 'react';
 import QUESTIONS from '../questions';
-import quizCompleteImg from '../assets/quiz-complete.png';
+
 import Question from './Question';
+import Summary from './Summary';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -17,12 +18,7 @@ export default function Quiz() {
   }, [handleSelectAnswer]);
 
   if (quizComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy Icon" />
-        <h2>Quiz Complete</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
